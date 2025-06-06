44dffb214d1211008112f84ad9ca1dff6b4598bc
dhanushiya <dhanushiyadhanush962@gmail.com>
2025-06-06 16:37:17 +0530

    made changes

---
+++ java script/script.js
@@ -0,0 +1,12 @@
+var marks=[100,100,100,100,100];
+var[m1,m2,m3,m4,m5]=marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+
+var a=10
+var b=20
+var sum=a+b;
+console.log(a,"+",b,"=",sum)
